da1f06495ac70da9eaefd179f98ea695254834fc
Prashant Singh <box15prashant@outlook.com>
2022-09-05 20:04:28 +0530

    Added PropertyUtils.java to read and load property files.

---
+++ src/test/java/com/testvagrant/utils/PropertyUtils.java
@@ -0,0 +1,33 @@
+package com.testvagrant.utils;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyUtils {
+
+    public static Properties propertyLoader(String filePath){
+        Properties properties = new Properties();
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+
+            try {
+                properties.load(reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to load properties Files.");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Properties file not found at "+filePath);
+        }
+
+        return properties;
+    }
+}
